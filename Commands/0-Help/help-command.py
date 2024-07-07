80cf44518290e85852b1b18c08585cc02e49f647
GokuPlay609 <120821054+GokuPlay609@users.noreply.github.com>
2024-07-07 14:45:00 +0530

    Update help-command.py

--- Commands/0-Help/help-command.py
+++ Commands/0-Help/help-command.py
@@ -7,12 +7,6 @@ def help(bot: ClassVar[Any], message= ClassVar[Any]) -> NoReturn:
 
         # Making reply markup for help
         Markups: ClassVar[Any] = telebot.util.quick_markup({
-            "Website": {
-                "url": "https://kourva.github.io/GroupCtrl"
-            },
-            "Channel": {
-                "url": "https://t.me/GroupCtrl"
-            },
             "Get on Private chat": {
                 "callback_data": f"Send_Help_Private@{user.id}"
             }
